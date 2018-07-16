74a739808b3817e570bf8702d94963b47e5c0233
Matthew Wolfe <mathyou.wolfe@gmail.com>
2018-07-16 11:27:56 -0400

    another fixer

--- firebase_testing/settings.py
+++ firebase_testing/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'testapp.apps.TestappConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
