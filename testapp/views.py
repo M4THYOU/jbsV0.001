2046e98488f978c8b796e6a815f012f8d69781b8
Matthew Wolfe <mathyou.wolfe@gmail.com>
2018-07-16 13:07:41 -0400

    coming soon page

--- testapp/views.py
+++ testapp/views.py
@@ -3,6 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def testview(request):
-    test_para = "This is some \"dynamic\" text from the view."
-
-    return render(request, 'test_page.html', {'test_p':test_para})
+    return render(request, 'coming_soon_base.html', {})
