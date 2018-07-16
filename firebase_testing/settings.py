ae2e470143d2ddb7495d287601eabc4a6d98d423
Matthew Wolfe <mathyou.wolfe@gmail.com>
2018-07-16 11:25:28 -0400

    fixing again

--- firebase_testing/settings.py
+++ firebase_testing/settings.py
@@ -56,7 +56,7 @@ HEROKU_DIR = os.path.dirname(__file__)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(os.path.join(HEROKU_DIR, 'templates')),],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
