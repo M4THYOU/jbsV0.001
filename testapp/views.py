b8633199e43da7d067eec7baad3b6c13fd285285
Matthew Wolfe <mathyou.wolfe@gmail.com>
2018-07-16 10:03:56 -0400

    new testapp

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def testview(request):
+    test_para = "This is some \"dynamic\" text from the view."
+
+    return render(request, 'test_page.html', {'test_p':test_para})
