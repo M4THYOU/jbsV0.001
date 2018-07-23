d442c6b7ed28b2d0c7d063fcd370459b8f77e608
Matthew Wolfe <mathyou.wolfe@gmail.com>
2018-07-23 10:11:35 -0400

    fixing api

--- firesdk/views.py
+++ firesdk/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_exempt
 
 from firesdk.firebaseconn import *
 from firesdk.serializers import UserSerializer
