cd4d6cb36ba9256f07c5f99943a496cae06fe698
m4thyou <mathyou.wolfe@gmail.com>
2019-08-01 09:44:27 -0400

    test2

---
+++ firebase_testing/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/matthewwolfe/Desktop/JBS/firebase_testing/firebase_testing/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
